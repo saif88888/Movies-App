65cfbf1123b76dda8e2bdf6755687f0301a3afbd
saif88888 <saifsaeedfms1@gmail.com>
2021-09-07 10:52:04 +0100

    feat: Create index.html

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const APIURL =
-	'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+	'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e3225379d10535ab222db77bee2fbdc6&page=1';
 
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI =
-	'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
+	'https://api.themoviedb.org/3/search/movie?&api_key=e3225379d10535ab222db77bee2fbdc6&query=';
 
 getMovies(APIURL);
 
